408fc27ec7083e52a59b6915b1b27b080f9dffb5
Patrik <patrik.golec@fh-kufstein.ac.at>
2022-01-26 12:05:44 +0100

    updated init script

--- mysql/init.sql
+++ mysql/init.sql
@@ -2,11 +2,12 @@ USE cs_db;
 
 CREATE TABLE IF NOT EXISTS positions
 (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     session_id NVARCHAR(255) NOT NULL,
     crazyflie_id NVARCHAR(255) NOT NULL,
     x FLOAT NOT NULL,
     y FLOAT NOT NULL,
     z FLOAT NOT NULL,
-    timestamp INT NOT NULL
+    timestamp INT NOT NULL,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB;
